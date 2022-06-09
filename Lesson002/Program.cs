0f048a15df4da5217c742eeb4848a319dcfbee35
Hart <Hartik1995@mail.ru>
2022-06-09 17:39:20 +0300

    add_new_folder_Lesson002_with_programs

---
+++ Lesson002/Program.cs
@@ -0,0 +1,37 @@
+﻿/* int n = new Random().Next(100, 1000);
+
+Console.WriteLine(n);
+
+int c1 = n / 100; // 1 цифра
+int c2 = n / 10 % 10; // 2 цифра
+int c3 = n % 10; // 3 цифра
+
+Console.WriteLine($"2 цифра - {c2}.  Первая и 3 цифра, без 2 цифры: {c1}{c3}"); */
+
+
+/* Напишите программу, которая будет принимать на
+вход два числа и выводить, является ли второе число
+кратным первому. Если число 2 не кратно числу 1, то
+программа выводит остаток от деления.
+ */
+
+/* int a = new Random().Next(100, 1000);
+int b = new Random().Next(100, 1000); */
+
+/* Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if (a % b == 0)
+{
+    Console.WriteLine("Yes");
+}
+else Console.WriteLine("No, mod " + a % b);
+
+ */
+
+ int a = new Random().Next(100, 1000);
+
+ Console.WriteLine(a);
+ string str = a.ToString();
+
