f5ab6226ff6c0a4e5ccc161cb9c227201d9f6b21
Hart <Hartik1995@mail.ru>
2022-06-14 09:51:04 +0300

    New folder - Self003 and Lesoon003

---
+++ Lesson003/Program.cs
@@ -0,0 +1,10 @@
+﻿/*  Напишите программу, которая принимает на вход
+координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+номер четверти плоскости, в которой находится эта
+точка. */
+
+Console.Writeln("Введите х");
+int X = int.Parse(Console.ReadLine());
+
+Console.Writeln("Введите y");
+int X = int.Parse(Console.ReadLine());
---
+++ SelfEducation002/Program.cs
@@ -0,0 +1,13 @@
+﻿//Self education
+
+
+int[] array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int valueToAdd = 10;
+int[] largerArray = new int[array.Length + 1];
+
+for (int i = 0; i < array.Length; i++)
+{
+    largerArray[i] = array[i];
+}
+largerArray[array.Length] = valueToAdd;
+array = largerArray;
