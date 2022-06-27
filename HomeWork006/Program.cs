fe652d5ff5eef2e46eaa8c5531f260af6a489766
Hart <Hartik1995@mail.ru>
2022-06-27 15:40:17 +0300

    add new folder and program

---
+++ HomeWork006/Program.cs
@@ -0,0 +1,47 @@
+﻿/*  Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3 */
+void PrintArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+int[] CreateRandomArray(int length)
+{
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(1, 300);
+    }
+    return arr;
+}
+int[] InputNumbersN(int length)
+{
+    int[] arr = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+    return arr;
+}
+
+
+Console.WriteLine("Введите размер массива");
+int[] arr = InputNumbersN(int.Parse(Console.ReadLine()));
+PrintArr(arr);
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    } 
+    if else (count == 0)
+}
+ Console.WriteLine(count);
---
+++ Lesson007/Program.cs
@@ -0,0 +1,51 @@
+﻿/* Задайте двумерный массив размером m×n,
+заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1 */
+/* 
+int m = 3, n = 4;
+int[,] mass = new int[m, n];
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = new Random().Next(0, 10);
+        mass [i, j] = i+j;
+    }
+}
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        Console.Write(mass[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+ */
+/* Задайте двумерный массив. Найдите элементы, у
+которых оба индекса чётные, и замените эти элементы на их
+квадраты. */
+
+
+int[] CreatRandomArray(int length)
+{
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(1, 10);
+    }
+    return arr;
+}
+void PrintArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
